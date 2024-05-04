7640c0530f6baf45cf81067e464e9ebd922618ef
Matthew Borchert <alphaom34@gmail.com>
2024-05-04 09:39:44 -0500

    postprocess unittest

--- src/tests/lex_test.rs
+++ src/tests/lex_test.rs
@@ -1,8 +1,9 @@
 use crate::lexer::{lex, TokenType};
+use crate::post_processor::post_process;
 use crate::read_file;
 
 #[test]
-fn test() {
+fn lex_test() {
     let path: &str = "test/lex.dym";
     assert_eq!(
         lex(&read_file(path)),
@@ -41,4 +42,49 @@ fn test() {
             TokenType::Name(String::from("name😄"))
         ]
     );
+}
+
+#[test]
+fn post_process_test() {
+    let path: &str = "test/postprocess.dym";
+    let lexed: &mut Vec<TokenType> = &mut lex(&read_file(path));
+    post_process(lexed);
+    assert_eq!(
+        *lexed,
+        [
+            TokenType::Name(String::from("foo")),
+            TokenType::Set,
+            TokenType::LeftFuncParenthesis,
+            TokenType::RightFuncParenthesis,
+            TokenType::Lambda,
+            TokenType::LeftCurlyBracket,
+            TokenType::LineFeed,
+            TokenType::Name(String::from("bar")),
+            TokenType::Set,
+            TokenType::LeftFuncParenthesis,
+            TokenType::RightFuncParenthesis,
+            TokenType::Lambda,
+            TokenType::LeftCurlyBracket,
+            TokenType::LeftCaptureParenthesis,
+            TokenType::Name(String::from("baz")),
+            TokenType::Add,
+            TokenType::Name(String::from("bonk")),
+            TokenType::RightCurlyBracket,
+            TokenType::RightCaptureParenthesis,
+            TokenType::LineFeed,
+            TokenType::RightCurlyBracket,
+            TokenType::LineFeed,
+            TokenType::Name(String::from("bop")),
+            TokenType::LeftCallParenthesis,
+            TokenType::LeftFuncParenthesis,
+            TokenType::Name(String::from("beep")),
+            TokenType::RightFuncParenthesis,
+            TokenType::Lambda,
+            TokenType::LeftCurlyBracket,
+            TokenType::Name(String::from("bope")),
+            TokenType::RightCurlyBracket,
+            TokenType::RightCallParenthesis,
+            TokenType::LineFeed,
+        ]
+    )
 }
