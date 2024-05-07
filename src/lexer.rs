0027cc22239ed88dd812c7fc82154409db742790
iamaspeedcuber1 <31186303-iamaspeedcuber1@users.noreply.replit.com>
2024-05-07 16:16:50 +0000

    changed lexer to identify tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,58 +2,83 @@ use std::vec::Vec;
 use regex::Regex;
 use regex::Match;
 
-pub struct Block {
-  block: Vec<String>,
+static IDENTIFIER_REGEX: &str = "[^\\d\\s+\\-*/\\^%=><\\\\&|~;:,.!?'\"`@()\\[\\]{}#][^\\s+\\-*/\\^%=><\\\\&|~;:,.!?'\"`@()\\[\\]{}#]*";
+static COMMENT_REGEX: &str = "(##(#?[^#])*##)|(#[^\n]*\n)";
+static SPACE_REGEX: &str = "([\\s^\n]|\n)+";
+static STRING_REGEX: &str = "`[^`]*`|'(\\\\.|[^'])'|\"(\\\\.|[^\"])\"";
+static SYMBOL_REGEX: &str = r"->|<?..<?|[?+\-:]{2}|([+\-*/%&|\^?!><]|[/*><]{2})=?|[$@=;:,.~()\[\]{}]";
+static DEC_REGEX: &str = r"(\d+(_(\d+))*)?\.?(\d+(_(\d+))*)+i?(e|E(\+|-)?\d+)?";
+static NUM_REGEX: &str = r"((0b[01]*\.?[01]+)|(0o[0-7]*\.?[0-7]+)|(0x[0-9a-fA-F]*\.?[0-9a-fA-F]+))i?";
+
+pub fn matches_regex(regex: &str, token: &str) -> bool {
+  return Regex::is_match(&Regex::new(regex).unwrap(), &token);
+}
+
+enum TokenType {
+  Identifier,
+  DecimalNum,
+  Number,
+  String,
+  GroupOpen,
+  GroupClose,
+  Symbol,
+  Whitespace,
+  LineFeed,
+  Comment
+}
+
+struct Token {
+  token: TokenType,
+  string: String
+}
+
+fn token_type(token: &str) -> TokenType {
+  if token == "\n" {
+    return TokenType::LineFeed;
+  } else if token.starts_with("#") {
+    return TokenType::Comment;
+  } else if "([{".contains(&token) {
+    return TokenType::GroupOpen;
+  } else if ")]}".contains(&token) {
+    return TokenType::GroupClose;
+  } else if token.trim().is_empty() {
+    return TokenType::Whitespace;
+  } else if matches_regex(STRING_REGEX, &token) {
+    return TokenType::String;
+  } else if matches_regex(DEC_REGEX, &token) {
+    return TokenType::DecimalNum;
+  } else if matches_regex(NUM_REGEX, &token) {
+    return TokenType::Number;
+  } else if matches_regex(SYMBOL_REGEX, &token) {
+    return TokenType::Symbol;
+  } return TokenType::Identifier;
+}
+
+struct Block {
+  block: Vec<Token>,
   end: usize
 }
 
 
-static NO_NAME: &str = "+\\-*/\\^%=><\\\\&|~;:,.!?'\"`@()\\[\\]{}#";
-static COMMENT_REGEX: &str = "(##(#?[^#])*##)|(#[^\n]*\n)";
-static SPACE_REGEX: &str = "([\\s^\n]|\n)+";
-static STRING_REGEX: &str = "`[^`]*`";
-static SYMBOL_REGEX: &str = r"->|[?+\-:.]{2}|([+\-*/%&|\^?!><]|[/*><]{2})=?|[@=;:,.~]";
-static DEC_REGEX: &str = r"(\d+(_(\d+))*)?\.?(\d+(_(\d+))*)+i?(e|E(\+|-)?\d+)?";
-static NUM_REGEX: &str = r"((0b[01]*\.?[01]+)|(0o[0-7]*\.?[0-7]+)|(0x[0-9a-fA-F]*\.?[0-9a-fA-F]+))i?";
-
-
-pub fn evaluate(code: &str) {
-  let mut st: &str = code;
-  while !st.is_empty() {
-    let line: Block = get_block(st);
-    let token: &str = &st[..line.end];
-
-    println!("{}", token);
-    st = &st[line.end..];
-  }
-}
-
-
-pub fn match_quote(code: &str) -> usize {
+/*pub fn match_quote(code: &str) -> usize {
   let quote: char = code.chars().nth(0).unwrap();
   let mut i: usize = 0;
-  let mut back: bool = false;
   
   while i < code.len() - 1 {
     i += 1;
     let current: char = code.chars().nth(i).unwrap();
-    if back {
-      back = false;
-      continue;
-    }
     if current == quote {
       break;
     } if current == '\\' {
-      back = true;
+      i += 1;
+      continue;
     } else if current == '{' {
       i += match_group(&code[i..], '}');
     }
   }
   return i;
-}
-
-
-pub fn match_group(code: &str, close: char) -> usize {
+}*/
+/*pub fn match_group(code: &str, close: char) -> usize {
   let open: char = code.chars().nth(0).unwrap();
   let mut i: usize = 0;
   let mut balance: i32 = 0;
@@ -85,31 +110,29 @@ pub fn match_group(code: &str, close: char) -> usize {
     i += 1;
   }
   return i;
-}
+}*/
 
 
-pub fn get_next(code: &str) -> usize {
-  if code.starts_with("\"") || code.starts_with("'") {
-    return 1 + match_quote(code);
-  } else if code.starts_with("(") {
-    return 1 + match_group(code, ')');
-  } else if code.starts_with("[") {
-    return 1 + match_group(code, ']')
-  } else if code.starts_with("{") {
-    return 1 + match_group(code, '}')
-  }
-  
-  let identifier_regex: &str = &format!(r"[^\d\s{}][^\s{}]*", NO_NAME, NO_NAME);
-  let token_regex: Regex = Regex::new(&format!("^({})|({})|({})|({})|({})|({})|({})", COMMENT_REGEX, SPACE_REGEX, DEC_REGEX, NUM_REGEX, STRING_REGEX, SYMBOL_REGEX, identifier_regex)).unwrap();
+// TODO someday: add support for nested formatted strings
+// (Python didn't add this until ver 3.12 lol)
+pub fn get_next(code: &str) -> Token {
+  let token_regex: Regex = Regex::new(&format!("^({})|({})|({})|({})|({})|({})|({})", COMMENT_REGEX, SPACE_REGEX, DEC_REGEX, NUM_REGEX, STRING_REGEX, SYMBOL_REGEX, IDENTIFIER_REGEX)).unwrap();
 
   let token: Option<Match> = Regex::find(&token_regex, code);
   if !token.is_none() {
-    return token.unwrap().end();
-  } return code.len();
+    let t: Match = token.unwrap();
+    if t.start() != 0 {
+      let string: &str = &code[..t.end()];
+      return Token {
+        token: token_type(string),
+        string: string.to_string()
+      };
+    }
+  } panic!("SyntaxError: invalid token");
 }
 
 
-pub fn get_block(code: &str) -> Block {
+/*pub fn get_block(code: &str) -> Block {
   let mut st: &str = code;
   let mut tokens: Vec<String> = Vec::new();
   let mut end: usize = 0;
@@ -144,4 +167,4 @@ pub fn get_block(code: &str) -> Block {
     block: tokens,
     end: end
   };
-}
+}*/
